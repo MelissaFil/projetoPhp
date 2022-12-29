5ab966af6b3aa68f55baf8d0723cbe59ca9591f8
GitMel <melissafilgueira8@gmail.com>
2022-12-29 20:10:02 -0300

    adicionando agencia no front

--- public/assets/js/usuarios.js
+++ public/assets/js/usuarios.js
@@ -1,7 +1,9 @@
 $(function(){
     $("#formAgencia").submit(function(e){
         e.preventDefault();
-        var nome = $(this).val()
-        console.log('oi')
+        var nome = $("#formAgencia input").val()
+        console.log($('.tbody').parent())
+        $('.tbody').append('<tr><th scope="row">1</th><td>'+nome+'</td></tr>')
+
     })
 })
