9f0342f77c965e7a7630315e97d183c88b1b2ac4
GitMel <melissafilgueira8@gmail.com>
2023-01-09 15:33:41 -0300

    retirando arquivos antigos e adicionando body a container

--- public/assets/js/usuarios.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $("#formAgencia").submit(function(e){
-        e.preventDefault();
-        var nome = $("#formAgencia input").val()
-        console.log($('.tbody').parent())
-        $('.tbody').append('<tr><th scope="row">1</th><td>'+nome+'</td></tr>')
-
-    })
-})
